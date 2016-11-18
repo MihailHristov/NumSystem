515ab94658cf95a86751c7b0290aac93539e2089
internet <todor.chupov@gmail.com>
2016-11-18 15:50:40 +0200

    Add project files.

---
+++ 01.DecimalToBinary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace _01.DecimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //condition: https://github.com/TelerikAcademy/CSharp-Part-2/blob/master/Topics/04.%20Numeral-Systems/homework/01.%20Decimal%20to%20binary/README.md
+            ulong inputDecimalNumb = ulong.Parse(Console.ReadLine());
+            StringBuilder result = new StringBuilder();
+            DecimalToBinary(inputDecimalNumb, result);
+            Console.WriteLine(result);
+        }
+
+        static void DecimalToBinary(ulong inputDecimalNumb, StringBuilder result)
+        {
+            ulong temp = 0;
+            while (inputDecimalNumb != 0)
+            {
+                temp = inputDecimalNumb % 2;
+                result.Append(temp);
+                inputDecimalNumb = inputDecimalNumb / 2;
+            }
+            Reverse(result);
+        }
+
+        static void Reverse(StringBuilder result)
+        {
+            StringBuilder temp = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                temp.Append(result[result.Length - i - 1]);
+            }
+            result.Clear();
+            result.Append(temp);
+        }
+    }
+}
---
+++ 02.BinaryToDecimal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02.BinaryToDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //condition: https://github.com/TelerikAcademy/CSharp-Part-2/blob/master/Topics/04.%20Numeral-Systems/homework/02.%20Binary%20to%20decimal/README.md
+            string inputBinary = Console.ReadLine();
+            long result = BinaryToDecimal(inputBinary);
+            Console.WriteLine(result);
+        }
+
+        static long BinaryToDecimal(string inputBinary)
+        {
+            int digit = 0;
+            int position = 0;
+            long tempResult = 0;
+            for (int i = 0; i < inputBinary.Length; i++)
+            {
+                digit = inputBinary[i] - '0'; //use "Unicode" table to return value (0 or 1), without "parsing"
+                position = inputBinary.Length - i - 1;
+                tempResult += digit * (long)Math.Pow(2, position);
+            }
+            return tempResult;
+        }
+    }
+}
